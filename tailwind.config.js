a682c9adf250c18e92d34f20c757928bd88f76e0
Osmanov <sinaverosmanovfrontend@gmail.com>
2023-07-25 03:25:14 +0300

    almost done

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
       "light-gray": "#E3E6EC",
       black: "#474955",
       white: "#FCFCFC",
-      "bg-input": "#5A5C66",
+      gray: "#5A5C66",
       green: "#7EBC3C",
       "gray-dark": "#B2B7BF",
     },
